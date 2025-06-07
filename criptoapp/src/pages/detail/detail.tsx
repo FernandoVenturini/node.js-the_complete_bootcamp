21f47c5bb573402bccf3b59db2d453f8655469ad
FernandoVenturini <fernandoventurini9@hotmail.com>
2025-06-07 12:15:16 +0100

    Delete criptoapp directory

--- criptoapp/src/pages/detail/detail.tsx
+++
@@ -1,7 +0,0 @@
-export function Detail(){
-    return (
-        <>
-            <h1>Pagina Detalhe da Moeda!</h1>
-        </>
-    );
-}
--- criptoapp/src/pages/notfound/notfound.tsx
+++
@@ -1,7 +0,0 @@
-export function Notfound() {
-    return (
-        <>
-            <h1>Pagina 404! Pagina nao encontrada!</h1>
-        </>
-    );
-};
