3de2b042133d1ab0c2d827073f1ad429fb362406
Fernando <fernandoventurini9@hotmail.com>
2025-06-08 20:05:18 +0100

    routing #03

--- index.js
+++ index.js
@@ -36,11 +36,23 @@ console.log('Will read file!');
 
 /********************************SERVER********************************** */
 const { createServer } = require('node:http');
+const path = require('node:path');
 const hostName = '127.0.0.1';
 const PORT = 8000;
 
 const server = createServer((req, res) => {
-    res.end('Hello from the server!');
+    const pathName = req.url;
+    if (pathName === '/' || pathName === '/overview') {
+        res.end('This is the OVERVIEW!');
+    } else if (pathName === '/product') {
+        res.end('This is the PRODUCT!');
+    } else {
+        res.writeHead(404, {
+            'content-type': 'text/html',
+            'my-own-header': 'hello-world'
+        });
+        res.end('<h1>Page not found!</h1>');
+    }
 });
 
 server.listen(8000, '127.0.0.1', () => {
